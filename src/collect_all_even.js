a6d1c1f6227ae65057ffbe318678f38a8b45bfb7
ZL1996 <649349296@qq.com>
2016-05-29 19:38:49 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -2,14 +2,11 @@
 
 function collect_all_even(collection) {
   var result = [];
-  for(var i=0;i<collection.length;i++)
-    result = judge(collection[i]);
+  for(var i=0;i<collection.length;i++){
+    if(collection[i]%2==0)
+       result.push(collection[i]);
+  }
   return result;
 }
 
-function judge(a,b){
-  if (a%2 == 0)
-    b.push(a);
-  return b;
-}
 module.exports = collect_all_even;
