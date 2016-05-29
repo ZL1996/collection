b4933ae6488904de0b2c950f8cb2ec42b92797c1
ZL1996 <649349296@qq.com>
2016-05-29 19:33:58 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
+  var result = [];
+  for(var i=0;i<collection.length;i++)
+    result = judge(collection[i]);
+  return result;
 }
 
+function judge(a,b){
+  if (a%2 == 0)
+    b.push(a);
+  return b;
+}
 module.exports = collect_all_even;
